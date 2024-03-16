5b93427d68c52ae8d51c6355ccc6df1beaf8c4d9
Corvin McPherson <corvin@corvin.dev>
2024-03-15 22:04:16 -0400

    Once again update mac syscall numbers

--- test/sc_hw_mac_x86_64.asm
+++ test/sc_hw_mac_x86_64.asm
@@ -1,14 +1,14 @@
 BITS 64
 
 _start:
-    mov rax, 4     ; write
+    mov rax, 185     ; write
     mov rdi, 1
     lea rsi, [rel msg]
     mov rdx, msg_len
     syscall
 exit:
     mov rdi, 0
-    mov rax, 1     ; exit
+    mov rax, 179     ; exit
     syscall
 msg:
     db  `Hello Shellcode!\n`
